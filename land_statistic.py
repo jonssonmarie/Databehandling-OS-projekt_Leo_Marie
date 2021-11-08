d254269004a7aaafd5e0def60f05fe8191a7ad2e
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-11-08 14:32:59 +0100

    col Name is anomynised

--- land_statistic.py
+++ land_statistic.py
@@ -4,12 +4,20 @@ Uppgift 1 - Landstatistik
 Ni får ett land tilldelat till er grupp. Börja med att anonymisera kolumnen med idrottarnas namn med
 hashfunktionen SHA-256.
 Undersök därefter hur det gått för landet i OS genom tiderna. Visualisera exempelvis:
-de sporter landet fått flest medaljer i
-antal medaljer per OS
-histogram över åldrar
+- de sporter landet fått flest medaljer i
+- antal medaljer per OS
+- histogram över åldrar
 Skapa fler plots för att visualisera flera aspekter kring ert land och dess sportprestationer i O
 """
 import pandas as pd
 import numpy as np
+import hashlib as hl
 from load_data import create_df
 
+athlete_event, noc_regions = create_df()
+
+
+athlete_event["Name"] = athlete_event["Name"].astype(str)
+athlete_event["Name"] = athlete_event["Name"].apply(lambda x: hl.sha256(x.encode()).hexdigest())
+
+#print(athlete_event["Name"].head())
