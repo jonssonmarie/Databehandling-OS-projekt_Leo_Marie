22431b8cd4f1968c7f1b1ca104f219652937b178
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-11-11 20:45:50 +0100

    upd docstrings

--- create_plot.py
+++ create_plot.py
@@ -8,14 +8,14 @@ import plotly
 
 def bar_plot(df, x_value, y_value, an_title, y_name, color_name, html_path):
     """
-    :param df: 
-    :param x_value: 
-    :param y_value: 
-    :param an_title: 
-    :param y_name: 
-    :param color_name: 
-    :param html_path: 
-    :return: 
+    :param df: dataframe 
+    :param x_value: The variable t to use for the x axis
+    :param y_value: The variable/s to use for the y axis
+    :param an_title: The title above the graph
+    :param y_name: To be able to set a specified y label name
+    :param color_name: the variable to use as legend
+    :param html_path: were the html offline plot is saved
+    :return: a plot
     """
     fig = px.bar(df, x=x_value, y=y_value, barmode='group', color=color_name,
                  labels={"variable": "", "value": y_name}, title=an_title)
@@ -24,14 +24,14 @@ def bar_plot(df, x_value, y_value, an_title, y_name, color_name, html_path):
 
 def histogram_plot(df, x_value, y_value, an_title, y_name, color_name, html_path):
     """
-    :param df: 
-    :param x_value: 
-    :param y_value: 
-    :param an_title: 
-    :param y_name: 
-    :param color_name: 
-    :param html_path: 
-    :return: 
+    :param df: dataframe 
+    :param x_value: The variable t to use for the x axis
+    :param y_value: The variable/s to use for the y axis
+    :param an_title: The title above the graph
+    :param y_name: To be able to set a specified y label name
+    :param color_name: the variable to use as legend
+    :param html_path: were the html offline plot is saved
+    :return: a plot
     """
     fig = px.histogram(df, x=x_value, y=y_value, barmode='group', color=color_name,
                        labels={"variable": "", "value": y_name}, title=an_title)
