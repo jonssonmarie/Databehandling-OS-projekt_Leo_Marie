5e92782104e0df94ab027e0dd17ebd629e6897e1
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-11-12 10:18:14 +0100

    Update create_plot.py

--- create_plot.py
+++ create_plot.py
@@ -41,18 +41,31 @@ def histogram_plot(df, x_value, y_value, an_title, y_name, color_name, html_path
 
 def horizontal_bar_plot(df, x_value, y_value, an_title, y_name, color_name, html_path, reversed_y=False):
     """
-    :param df: 
-    :param x_value: 
-    :param y_value: 
-    :param an_title: 
-    :param y_name: 
-    :param color_name: 
-    :param html_path: 
-    :param reversed_y: 
-    :return: 
+    :param df: dataframe
+    :param x_value: The variable/s to use for the x axis
+    :param y_value: The variable to use for the y axis
+    :param an_title: The title above the graph
+    :param y_name: To be able to set a specified y label name
+    :param color_name: the variable to use as legend
+    :param html_path: were the html offline plot is saved
+    :param reversed_y: Bool, default False, allows to invert y axis values.
+    :return: a plot, with barmode = stack  
     """
     fig = px.bar(df, x=x_value, y=y_value, barmode='stack', orientation='h', color=color_name,
                  labels={"variable": "", "value": y_name}, title=an_title)
     if reversed_y:
         fig.update_layout(yaxis=dict(autorange="reversed"))
+    plotly.offline.plot(fig, filename=html_path)
+
+
+def pie_chart(df, values, names, an_title, html_path):
+    """
+    :param df: dataframe
+    :param values: The variable to be displayed by the chart
+    :param names: the variable to use as legend
+    :param an_title: The title above the graph
+    :param html_path: were the html offline plot is saved
+    :return: a plot
+    """
+    fig = px.pie(df, values = values, names= names, title = an_title)
     plotly.offline.plot(fig, filename=html_path)
