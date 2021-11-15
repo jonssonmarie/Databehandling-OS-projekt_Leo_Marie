c7298addb63b13533b692ed43e5a69fc9ff14c4e
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-11-15 11:37:14 +0100

    small fix

--- main.py
+++ main.py
@@ -2,32 +2,24 @@ import time
 
 from land_statistic import  sort_france_medals, count_medals_per_sport, medals_per_os
 from load_data import create_df
-from create_plot import histogram_plot, bar_plot, pie_chart, horizontal_bar_plot
+from create_plot import bar_plot, bar_plot_dash, pie_chart_dash, horizontal_bar_plot_dash
 #from sort_data import athletes_by_sex_ratio, athletes_by_sex_ratio_over_time, top_10_nations_medals
 
+
 # call_plot skall bort senare när olympic_app är klar
-def call_plot(df1, df5, df6, df7, df8,df9, df10, df11, df12):  # tillfällig att det är df1, df2 ... df6
-    histogram_plot(df1, "Age", "Amount", "Amount of medals per age for France in OS", "Amount", None,
-                   r"..\Visualizations\France_amount_medals_per_age.html")
-
-    bar_plot(df5,"Sport", ["Amount M"], "Male medal per sport, France in OS", "Amount", "Medal",
-             r"..\Visualizations\France_medals_per_man_sport.html")
-
-    bar_plot(df6, "Sport", ["Amount F"], "Female medal per sport, France in OS", "Amount", "Medal",
-             r"..\Visualizations\France_medals_per_female_sport.html")
-    
-    pie_chart(df7, "Count", "Sex", "Historical ratio males-females",
+def call_plot(df7, df8,df9, df10, df11, df12):  # tillfällig att det är df1, df2 ... df6
+    pie_chart_dash(df7, "Count", "Sex", "Historical ratio males-females",
              "../Visualizations/sex_pie.html")
 
-    bar_plot(df8, "Nation", "Total", "Top 10 Nations, total medals, winter-summer olympics", "count medals", None)
+    bar_plot_dash(df8, "Nation", "Total", "Top 10 Nations, total medals, winter-summer olympics", "count medals", None)
     
-    bar_plot(df9, "Nation", "Total", "Top 10 Nations, total medals, summer olympics", "count medals", None)
+    bar_plot_dash(df9, "Nation", "Total", "Top 10 Nations, total medals, summer olympics", "count medals", None)
         
-    bar_plot(df10, "Nation", "Total", "Top 10 Nations, total medals, winter olympics", "count medals", None)
+    bar_plot_dash(df10, "Nation", "Total", "Top 10 Nations, total medals, winter olympics", "count medals", None)
 
-    horizontal_bar_plot(df11, ['Share M','Share F'], 'Year', "Historical repartition of athletes by gender under winter olympics", "Share", None, True)
+    horizontal_bar_plot_dash(df11, ['Share M','Share F'], 'Year', "Historical repartition of athletes by gender under winter olympics", "Share", None, True)
 
-    horizontal_bar_plot(df12, ['Share M','Share F'], 'Year', "Historical repartition of athletes by gender under summer olympics", "Share", None, True)
+    horizontal_bar_plot_dash(df12, ['Share M','Share F'], 'Year', "Historical repartition of athletes by gender under summer olympics", "Share", None, True)
 
 
 
