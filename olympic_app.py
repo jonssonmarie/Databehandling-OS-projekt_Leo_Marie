135aba9365420da9558689ee693695192eccbc41
Lelelass <82147263+Lelelass@users.noreply.github.com>
2021-11-15 13:52:46 +0100

    Update olympic_app.py
    Added two columns to all nations

--- olympic_app.py
+++ olympic_app.py
@@ -8,8 +8,9 @@ from dash.dependencies import Output, Input
 
 
 from land_statistic import anonymous, age_per_os, sort_france_medals, count_medals_per_sport, medals_per_os
+from sort_data import athletes_by_sex_ratio, top_10_nations_medals, athletes_by_sex_ratio_over_time
 
-from create_plot import histogram_plot, bar_plot, pie_chart, horizontal_bar_plot, bar_plot_dash, histogram_plot_dash
+from create_plot import histogram_plot, bar_plot, pie_chart_dash, horizontal_bar_plot_dash, bar_plot_dash, histogram_plot_dash
 #from sort_data import athletes_by_sex_ratio, athletes_by_sex_ratio_over_time, top_10_nations_medals
 import os
 
@@ -41,27 +42,58 @@ medals_os = medals_per_os(medals_france)
 age_os = age_per_os(medals_france)
 man_france = medals_sport[medals_sport["Amount M"] > 0]
 female_france = medals_sport[medals_sport["Amount F"] > 0]
+sex_ratio = athletes_by_sex_ratio(athlete_event)
+all_time_top_10 = top_10_nations_medals(athlete_event)
+sex_ratio_over_years = athletes_by_sex_ratio_over_time(athlete_event)
+
+
+stylesheets = [dbc.themes.MATERIA]
 
 dff_dict = dict(df1 = "age_os", df2 = "man_france", df3 = "female_france")
+dff_dict_set_2 = dict(df4 = "sex_ratio", df5 = "all_time_top_10", df6 = "sex_ratio_over_years")
 
 dff_options_dropdown = [{"label": symbol, "value": name}
                           for symbol, name in dff_dict.items()]
 
-app = dash.Dash(__name__)   # skapar server
+dff_options_dropdown_all = [{"label": symbol, "value": name}
+                          for symbol, name in dff_dict_set_2.items()]
+
+season_options = [{"label": option , "value": option}
+                for option in ["Summer", "Winter", "all"]]
+
+app = dash.Dash(__name__, external_stylesheets=stylesheets,
+                meta_tags=[dict(name="viewport", content="width=device-width, initial-scale=1.0")])
 
 app.layout = dbc.Container([
     dbc.Row([
-        html.H1("Test"),
+        html.H1("Frankrike"),
         dcc.Dropdown(id = "os-dropdown", options=dff_options_dropdown, value="age_os"),
         dcc.Graph(id = "graph"),
         dcc.Store(id = "local-store")
     ]),
 
+    
     dbc.Row([
-        html.H1("Leos grejer"),
-        dcc.Dropdown(id = "leo-dropdown", options=dff_options_dropdown, value="age_os"),
-        dcc.Graph(id = "graph2"),
-        dcc.Store(id = "local-store2")
+
+        html.H2("All nations"),
+
+        dbc.Col([
+            dcc.Dropdown(id = "leo-dropdown", options=dff_options_dropdown_all, value="sex_ratio"),
+        ], width=6, lg=3), #xs="12", sm="12", md="12", lg='4', xl="3"),
+
+        dbc.Col([
+            dbc.Card([
+            dcc.RadioItems(id='season_radio', className="m-1",
+                                options=season_options,
+                                value='all'
+                            ),
+            ])
+        ], width=6, lg=3)  #xs="12", sm="12", md="12", lg='4', xl="3")      
+    ]),
+
+    dbc.Row([
+    dcc.Graph(id = "graph2"),
+    dcc.Store(id = "local-store2")
     ])
 ])
 
@@ -80,6 +112,22 @@ def update_graph_1(data):
     elif data == "female_france":
         dff = female_france
         figure = bar_plot_dash(dff, "Sport", "Amount F", "Female medal per sport, France in OS", "Amount", "Medal")
+    return figure
+
+@app.callback(
+    Output('graph2', 'figure'),
+    Input("leo-dropdown", 'value'),
+)
+def update_graph_2(data):
+    if data == "sex_ratio":
+        dff = sex_ratio
+        figure =  pie_chart_dash(dff, "Count", "Sex", "Historical ratio males-females")
+    elif data == "all_time_top_10":
+        dff = all_time_top_10
+        figure = bar_plot_dash(dff, "Nation", "Total", "Top 10 Nations, total medals, winter-summer olympics", "count medals", None)
+    elif data == "sex_ratio_over_years":
+        dff = sex_ratio_over_years
+        figure = horizontal_bar_plot_dash(dff, ['Share M','Share F'], 'Year', "Historical repartition of athletes by gender under winter olympics", "Share", None, True)
     return figure
 
 
