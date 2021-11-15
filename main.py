f56f935b530673933737bb198de1ce4994d9da52
jonssonmarie <89392551+jonssonmarie@users.noreply.github.com>
2021-11-15 11:33:48 +0100

    small fix due to app

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import time
 
-from land_statistic import anonymous, age_per_os, sort_france_medals, count_medals_per_sport, medals_per_os
+from land_statistic import  sort_france_medals, count_medals_per_sport, medals_per_os
 from load_data import create_df
 from create_plot import histogram_plot, bar_plot, pie_chart, horizontal_bar_plot
-from sort_data import athletes_by_sex_ratio, athletes_by_sex_ratio_over_time, top_10_nations_medals
+#from sort_data import athletes_by_sex_ratio, athletes_by_sex_ratio_over_time, top_10_nations_medals
 
+# call_plot skall bort senare när olympic_app är klar
 def call_plot(df1, df5, df6, df7, df8,df9, df10, df11, df12):  # tillfällig att det är df1, df2 ... df6
     histogram_plot(df1, "Age", "Amount", "Amount of medals per age for France in OS", "Amount", None,
                    r"..\Visualizations\France_amount_medals_per_age.html")
@@ -18,20 +19,15 @@ def call_plot(df1, df5, df6, df7, df8,df9, df10, df11, df12):  # tillfällig att
     pie_chart(df7, "Count", "Sex", "Historical ratio males-females",
              "../Visualizations/sex_pie.html")
 
-    bar_plot(df8, "Nation", "Total", "Top 10 Nations, total medals, winter-summer olympics", "count medals", None, 
-            "../Visualizations/All_nations_all_seasons_medal_top10" )
+    bar_plot(df8, "Nation", "Total", "Top 10 Nations, total medals, winter-summer olympics", "count medals", None)
     
-    bar_plot(df9, "Nation", "Total", "Top 10 Nations, total medals, summer olympics", "count medals", None, 
-            "../Visualizations/All_nations_summer_medal_top10" )
+    bar_plot(df9, "Nation", "Total", "Top 10 Nations, total medals, summer olympics", "count medals", None)
         
-    bar_plot(df10, "Nation", "Total", "Top 10 Nations, total medals, winter olympics", "count medals", None, 
-            "../Visualizations/All_nations_winter_medal_top10" )
+    bar_plot(df10, "Nation", "Total", "Top 10 Nations, total medals, winter olympics", "count medals", None)
 
-    horizontal_bar_plot(df11, ['Share M','Share F'], 'Year', "Historical repartition of athletes by gender under winter olympics", "Share", None, 
-            "../Visualizations/Athletes_by_gender_winter_years.html", True)
+    horizontal_bar_plot(df11, ['Share M','Share F'], 'Year', "Historical repartition of athletes by gender under winter olympics", "Share", None, True)
 
-    horizontal_bar_plot(df12, ['Share M','Share F'], 'Year', "Historical repartition of athletes by gender under summer olympics", "Share", None,
-     "../Visualizations/Athletes_by_gender_summer_years.html", True)
+    horizontal_bar_plot(df12, ['Share M','Share F'], 'Year', "Historical repartition of athletes by gender under summer olympics", "Share", None, True)
 
 
 
@@ -49,29 +45,25 @@ def offline_plot(df2, df3, df4):
 def main():
     start = time.time()
     athlete_event, noc_regions = create_df()
-    #Leos variabler
-    sex_ratio = athletes_by_sex_ratio(athlete_event)
+
+    # Leos variabler
+    """sex_ratio = athletes_by_sex_ratio(athlete_event)
     all_time_top_10 = top_10_nations_medals(athlete_event)
     winter_top_10 = top_10_nations_medals(athlete_event, "Winter")
     summer_top_10 = top_10_nations_medals(athlete_event, "Summer")
     sex_years_winter = athletes_by_sex_ratio_over_time(athlete_event, "Winter")
-    sex_years_summer = athletes_by_sex_ratio_over_time(athlete_event, "Summer")
+    sex_years_summer = athletes_by_sex_ratio_over_time(athlete_event, "Summer")"""
 
-
-    # Note: anonymous name                                  
-    athlete_event = anonymous(athlete_event)
+    # Note: anonymous name
     medals_france = sort_france_medals(athlete_event)
-    medals_sport = count_medals_per_sport(medals_france)   
+    medals_sport = count_medals_per_sport(medals_france)
     medals_os = medals_per_os(medals_france)
-    age_os = age_per_os(medals_france)
-    man_france = medals_sport[medals_sport["Amount M"] > 0]   
-    female_france = medals_sport[medals_sport["Amount F"] > 0]    
-    call_plot(age_os, man_france, female_france, sex_ratio, all_time_top_10,
-              winter_top_10, summer_top_10, sex_years_winter, sex_years_summer)
     offline_plot(medals_sport,medals_os, medals_france)
+
     end = time.time()
     print("\nThe time of execution of above program is :", end - start)
 
 
 if __name__ == '__main__':
     main()
+ 
